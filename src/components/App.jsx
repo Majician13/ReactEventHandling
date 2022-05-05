0eaae468f6efb9761a880a6ae69b4e2c2747709b
Majician13 <44847744+Majician13@users.noreply.github.com>
2022-05-05 14:58:31 -0400

    started course

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 function App() {
+  function handleClick() {
+    console.log("Clicked");
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
